05550265e9db8b61f9e432888046187d283cffe1
JAlexCarney <JAlexCarney@gmail.com>
2021-04-09 10:02:08 -0500

    Created Empty Week Two Project

---
+++ WeekTwoAssesment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeekTwoAssesment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
