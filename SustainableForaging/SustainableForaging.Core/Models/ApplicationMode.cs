63572b3fc86ce69359d23e4ae9465bad804ce10d
JAlexCarney <JAlexCarney@gmail.com>
2021-04-26 09:01:04 -0500

    Added Ninject

---
+++ SustainableForaging/SustainableForaging.Core/Models/ApplicationMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SustainableForaging.Core.Models
+{
+    public enum ApplicationMode
+    {
+        Live,
+        Test
+    }
+}
---
+++ SustainableForaging/SustainableForaging.UI/NinjectContainer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+using SustainableForaging.Core.Models;
+using SustainableForaging.Core.Repositories;
+using SustainableForaging.DAL;
+
+namespace SustainableForaging.UI
+{
+    class NinjectContainer
+    {
+        public static StandardKernel Kernel { get; private set; }
+
+        public static void Configure(ApplicationMode mode, string file1 = "", string file2 = "", string file3 = "") 
+        {
+            Kernel = new StandardKernel();
+
+            if (mode == ApplicationMode.Live)
+            {
+                Kernel.Bind<IForageRepository>().To<ForageFileRepository>().WithConstructorArgument(file1);
+                Kernel.Bind<IForagerRepository>().To<ForagerFileRepository>().WithConstructorArgument(file2);
+                Kernel.Bind<IItemRepository>().To<ItemFileRepository>().WithConstructorArgument(file3);
+            }
+            else if (mode == ApplicationMode.Test) 
+            {
+                // not implemented
+            }
+        }
+    }
+}
